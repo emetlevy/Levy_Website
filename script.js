207193359d7cf53b887481abe4b6f51dd7b34b42
emetlevy <76787870+emetlevy@users.noreply.github.com>
2021-09-05 14:04:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+
+//Enlarging images on click 
+
+  img = document.getElementById("Container");
+        // Function to set image dimensions
+        function enlargeImg() {
+            img.style.width = "60%";
+            img.style.height = "auto";
+            img.style.transition = "width 0.5s ease";
+
+        }
+        // Function to reset image dimensions
+        function resetImg() {
+            img.style.width = "40%";
+            img.style.height = "auto";
+            img.style.transition = "width 0.5s ease";
+            document.getElementById("Button").style.display= "none";
+        }
+
+        function showbutton(){
+            var x = document.getElementById("Button");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }
+
