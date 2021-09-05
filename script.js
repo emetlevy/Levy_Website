5a115f9adea4c6ff17e20bad4898f4e34500641d
emetlevy <76787870+emetlevy@users.noreply.github.com>
2021-09-05 14:05:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function showDivs(n) {
 
 //Enlarging images on click 
 
-  img = document.getElementById("myslides");
+  img = document.getElementById("mySlides");
         // Function to set image dimensions
         function enlargeImg() {
             img.style.width = "60%";
